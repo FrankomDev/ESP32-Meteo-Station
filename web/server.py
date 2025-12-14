f777d6a36b909e313c42d67b64a62e2fb0d5be25
FrankomDev <vasylopozdro@gmail.com>
2025-12-14 18:18:51 +0100

    merge web repo into this one

---
+++ web/server.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template, request
+import sqlite3
+import json
+from datetime import datetime
+
+app = Flask(__name__)
+
+def configure_db() -> None:
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS temperature "+
+                   "(id num, temperature num, pressure num, humidity num, rain num, day num, month num, year num, hour num, minute num)")
+    connection.commit()
+    connection.close()
+
+def get_index() -> int:
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    num = cursor.execute('SELECT id FROM temperature ORDER BY id DESC').fetchone()
+    connection.commit()
+    connection.close()
+    if num is None:
+        return 0
+    return int(num[0])+1
+
+def save_to_db(temperature, pressure, humidity, rain) -> None:
+    num = get_index()
+    time = datetime.now() #.strftime('%d.%m.%Y %H:%M')
+    print(time)
+    day, month, year = time.strftime("%d"), time.strftime("%m"), time.strftime("%Y")
+    hour, minute = time.strftime("%H"), time.strftime("%M")
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO temperature VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                   (num, temperature, pressure, humidity, rain, day, month, year, hour, minute))
+    connection.commit()
+    connection.close()
+
+def get_db() -> dict:
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    data = cursor.execute('SELECT * FROM temperature ORDER BY id DESC').fetchone()
+    connection.close()
+    return data
+
+@app.route('/')
+def index():
+    data = get_db()
+    date = f"{data[5]}.{data[6]}.{data[7]} {data[8]}:{data[9]}"
+    is_raining = "Nie"
+    if data[4] == 1:
+        is_raining = "Tak"
+    return render_template('index.html', date=date, temp=data[1], press=data[2], hum=data[3], rain=is_raining)
+
+@app.route('/api/get1h')
+def get1h():
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    data = cursor.execute('SELECT * FROM temperature ORDER BY id DESC LIMIT 4').fetchall()
+    connection.close()
+    return data
+
+@app.route('/api/send', methods=['POST'])
+def send():
+    data = request.get_data()
+    data = json.loads(data)
+    temperature = round(data['temperature'], 2)
+    pressure = round(data['pressure'], 1)
+    humidity = round(data['humidity'], 1)
+    rain = data['rain']
+    save_to_db(temperature, pressure, humidity, rain)
+    print( f"{temperature},  {pressure},  {humidity}, {rain}" )
+    return f"{temperature},  {pressure},  {humidity}, {rain}"
+
+if __name__ == '__main__':
+    configure_db()
+    app.run(debug=True,port=1111,host='0.0.0.0')
---
+++ web/static/gencharts.js
@@ -0,0 +1,83 @@
+const ctx = document.getElementById("temp1h");
+const ctx2 = document.getElementById("press1h");
+const ctx3 = document.getElementById("hum1h");
+
+fetch("/api/get1h")
+  .then((response) => response.json())
+  .then((data) => {
+    createChart(data);
+    //console.log(data);
+  });
+
+function createChart(data) {
+  let date1 = data[3][8]+":"+data[3][9];
+  let date2 = data[2][8]+":"+data[2][9];
+  let date3 = data[1][8]+":"+data[1][9];
+  let date4 = data[0][8]+":"+data[0][9];
+  new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: [date1, date2, date3, date4],
+      datasets: [
+        {
+          label: "Temperatura °C",
+          data: [data[3][1], data[2][1], data[1][1], data[0][1]],
+          borderWidth: 1,
+          backgroundColor: "#466EFD",
+          borderColor: "#466EFD",
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: false,
+        },
+      },
+    },
+  });
+  new Chart(ctx2, {
+    type: "line",
+    data: {
+      labels: [date1, date2, date3, date4],
+      datasets: [
+        {
+          label: "Ciśnienie hPa",
+          data: [data[3][2], data[2][2], data[1][2], data[0][2]],
+          borderWidth: 1,
+          backgroundColor: "#F6C109",
+          borderColor: "#F6C109",
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: false,
+        },
+      },
+    },
+  });
+  new Chart(ctx3, {
+    type: "line",
+    data: {
+      labels: [date1, date2, date3, date4],
+      datasets: [
+        {
+          label: "Wilgotność %",
+          data: [data[3][3], data[2][3], data[1][3], data[0][3]],
+          borderWidth: 1,
+          backgroundColor: "#248754",
+          borderColor: "#248754",
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: false,
+        },
+      },
+    },
+  });
+}
